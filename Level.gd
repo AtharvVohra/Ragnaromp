1af6d77fc5ede2330d1ff8442375b3a44b8f3454
Hugh Bagan <hbagan@ualberta.ca>
2018-11-10 16:04:02 -0700

    Added stub player node and level script

---
+++ Level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+onready var width = get_viewport().size.x
+onready var height = get_viewport().size.y
+
+func _ready():
+	print(width)
+	print(height)
+	
+func _update():
+	pass
+	
+func _process():
+	pass
+	
+func _physics_process(delta):
+	pass
