862fb50d18c0ddabc9bc1b397e8e20731573c2b0
Atharv Vohra <atharv@ualberta.ca>
2018-11-10 17:24:51 -0700

    Added draugr and icegiant nodes

---
+++ IceGiant.gd
@@ -0,0 +1,32 @@
+extends Node
+
+# class member variables go here, for example:
+var lane
+var isHit 
+var isReached 
+var xPos
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# randomizes draugr lane
+	lane = global.lanes[randi() % 3]
+	isHit = false
+	isReached = false
+	pass
+
+func _process(delta):
+	
+	if(isHit):
+		# change sprite
+		# add honor points
+		# play splat sound effect
+		global.honor += 4
+		pass
+	
+	if(isReached):
+		# decrease honor
+		# play people screaming sound effect
+		global.honor -= 2
+		pass
+	
+	pass
--- globals.gd
+++ globals.gd
@@ -4,7 +4,10 @@ onready var width = get_viewport().size.x
 onready var height = get_viewport().size.y
 
 # Later we should calculate the lane y-coords based on window height
+# draugrs add 2 honor
+# ice giants add 4 honor
 var lanes = [100, 200, 300]
-
+var playerHonor = 0
+var highScoreHonor = 0
 #func _ready():
 #	pass
