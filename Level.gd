fe3d5752c6c3a2e4c87cc76b4f7400a9144eb8cc
Hugh Bagan <hbagan@ualberta.ca>
2018-11-10 17:59:17 -0700

    Prototype player lane changing

--- Level.gd
+++ Level.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 onready var player = get_node("Player")
 var player_lane
+var target_lane
+var changing_lanes
+var delta_y
+var steps_to_move
+var delta_y_per_step
 
 func _ready():
 	print(globals.width)
@@ -16,17 +21,42 @@ func _ready():
 	print(player.position.y)
 	player.show()
 	
+	changing_lanes = false
+	steps_to_move = 20
+	
 func _process(delta):
 	# Input
-	if Input.is_action_just_pressed('ui_up'):
+	if (!changing_lanes and Input.is_action_just_pressed('ui_up')):
 		if (player_lane > 0):
-			player_lane -= 1
-	if Input.is_action_just_pressed('ui_down'):
+			target_lane = player_lane - 1
+			delta_y = globals.lanes[target_lane] - globals.lanes[player_lane]
+			delta_y_per_step = delta_y / steps_to_move
+			changing_lanes = true
+			print("Changing lanes to ")
+			print(target_lane)
+			print(delta_y)
+			print(delta_y_per_step)
+	if (!changing_lanes and Input.is_action_just_pressed('ui_down')):
 		if (player_lane < 2):
-			player_lane += 1
-
+			target_lane = player_lane + 1
+			delta_y = globals.lanes[target_lane] - globals.lanes[player_lane]
+			delta_y_per_step = delta_y / steps_to_move
+			changing_lanes = true
+			print("Changing lanes to ")
+			print(target_lane)
+			print(delta_y)
+			print(delta_y_per_step)
 	# Set objects' coords to their lane
-	player.position.y = globals.lanes[player_lane]
+	#player.position.y = globals.lanes[player_lane]
+	
+	if changing_lanes:
+		player.position.y += delta_y_per_step
+		print(player.position.y)
+		if (player.position.y == globals.lanes[target_lane]):
+			print("Finished changing lanes")
+			player_lane = target_lane
+			changing_lanes = false
+			
 
 #func _physics_process(delta):
 #	pass
