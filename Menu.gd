fec9cc014a1ad69483b06cd6aa547085c8ef8a7f
Atharv Vohra <atharv@ualberta.ca>
2018-11-11 09:05:24 -0700

    Added title screen, button works

---
+++ Menu.gd
@@ -0,0 +1,23 @@
+extends VBoxContainer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_StartGame_pressed():
+	get_tree().change_scene("res://Main.tscn")
+
+
+func _on_Upgrades_pressed():
+	pass # replace with function body
