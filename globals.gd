8f6680a035c07d4cd028e454332fe6cc27af7c2a
Hugh Bagan <hbagan@ualberta.ca>
2018-11-10 18:10:10 -0700

    Fixed lane changing; works for any lane ycoords

--- globals.gd
+++ globals.gd
@@ -4,7 +4,7 @@ onready var width = get_viewport().size.x
 onready var height = get_viewport().size.y
 
 # Later we should calculate the lane y-coords based on window height
-var lanes = [100, 200, 300]
+var lanes = [200, 350, 500]
 
 #func _ready():
 #	pass
