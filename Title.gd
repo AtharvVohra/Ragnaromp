64935c2fc755abeb8aa6946ae5c4970df7d6960d
Atharv Vohra <atharv@ualberta.ca>
2018-11-12 09:32:17 -0700

    end state

--- Title.gd
+++ Title.gd
@@ -8,6 +8,8 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	get_node("Won").set_text(String(globals.playerHonor)+"/200000")
+	if(globals.playerHonor >= 200000):
+		$Won.set_text(String("THOR SAVED ASGARD THANKS TO HIS HONOR! GAME LOGIC!"))
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
